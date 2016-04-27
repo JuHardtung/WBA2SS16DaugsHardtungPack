9bf9ff6630e3a92811bfe97ed40ac785b7a44a65
Trojan13 <trojan9418@gmail.com>
2016-04-27 14:18:00 +0200

    Aufgabe 1
    Print unsorted

---
+++ TimPack/w1/app.js
@@ -0,0 +1,2 @@
+var fs = require('fs');
+fs.readFile("./wolkenkratzer.json", function (e, d) {for (i in pD = JSON.parse(d).wolkenkratzer) {console.log("Name: ", pD[i].name,"\nStadt: ", pD[i].stadt,"\nHöhe: ", pD[i].hoehe + "m\n","\n--------------------\n");}});
