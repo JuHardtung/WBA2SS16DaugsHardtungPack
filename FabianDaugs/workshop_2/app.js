6fe48791ee80163f5c8c6c9ca8c5ce759b5e56d9
fdaugs <fabian.daugs@smail.th-koeln.de>
2016-05-17 12:28:51 +0200

    post methode hinzugefügt

--- FabianDaugs/workshop_2/app.js
+++ FabianDaugs/workshop_2/app.js
@@ -1,9 +1,11 @@
 var express = require('express');
-var bodyparser = require('body-parser');
+var bodyParser = require('body-parser');
 
 var app = express();
+var jsonParser = bodyParser.json();
 
-app.use(express.static(_dirname+ '/public'));
+
+app.use(express.static('./public'));
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
@@ -14,3 +16,20 @@ app.use(function(req,res,next){
   console.log('Time: %d '+' Request-Pfad: '+req.path, Date.now());
   next();
 });
+
+
+app.get('/warenkorb', function(req, res){
+
+    res.writeHead(200, "OK");
+    res.write('jooooo');
+    res.end();
+});
+
+
+app.post('/users', jsonParser, function (req, res){
+  res.writeHead(200, "OK");
+  res.write('done');
+  res.end();
+});
+
+app.listen(3000);
