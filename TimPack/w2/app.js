c535d2e001547b5b8a279e5a54e95bc4e9a4c147
Trojan13 <trojan9418@gmail.com>
2016-05-17 12:08:27 +0200

    shizzle

--- TimPack/w2/app.js
+++ TimPack/w2/app.js
@@ -1,3 +1,19 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
 app.listen(1335);
+
+app.use(express.static('./public'));
+
+
+app.use(function(req,res,next) {
+    console.log('Time: %d ' + ' Req-Path: ' + req.path, Date.now());
+    next();
+});
+
+app.get('/warenkorb', function(req, res){
+    
+    res.writeHead(200, "OK");
+    res.write('jooooo');
+    res.end();
+});
