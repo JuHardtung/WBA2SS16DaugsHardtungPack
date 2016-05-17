3b02d3df4f1fb2839cf9b6b9b9d8a1f0f6c6f3b5
Trojan13 <trojan9418@gmail.com>
2016-05-17 13:19:35 +0200

    Shizzle da Bizzle

--- TimPack/w2/app.js
+++ TimPack/w2/app.js
@@ -1,36 +1,36 @@
-var express = require('express');
-var bodyParser = require('body-parser')
-var app = express();
+var express        =         require("express");
+var bodyParser     =         require("body-parser");
+var app            =         express();
 
-
-app.use(express.static('./public'));
-
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(function(req,res,next) {
     console.log('Time: %d ' + ' Req-Path: ' + req.path, Date.now());
     next();
 });
 
-app.get('/warenkorb/:id', function(req, res){
-    
+
+app.get('/warenkorb/:id', function(req, res){  
     if (req.params.id == 42) {
-         res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ a: 1 }, null, 3));
-        
-        
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ money: 516 }, null, 3));    
     } else {
         res.write('User not found.');
     }
-   
-    
     res.end();
 });
 
-app.post('/kaufen',bodyParser, function(req,res){
-    
-    
-    
+
+app.post('/login',function(req,res){
+    var user_name=req.body.user;
+    var password=req.body.password;
+    console.log("User name = "+user_name+", password is "+password);
+    if (user_name == "test" && password == "123456789") {
+        console.log('Welcome!');
+    }
+    res.end();
 });
 
-
-app.listen(1335);
+app.listen(3000,function(){
+  console.log("Listing on port 3000");
+})
