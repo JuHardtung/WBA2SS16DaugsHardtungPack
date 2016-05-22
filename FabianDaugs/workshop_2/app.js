abcbe2eca727306f7831ed41e5f093877f3ff969
JuHardtung <julian.hardtung@smail.fh-koeln.de>
2016-05-22 03:33:17 +0200

    added GET method for stock filtering

--- FabianDaugs/workshop_2/app.js
+++ FabianDaugs/workshop_2/app.js
@@ -4,33 +4,58 @@ var bodyParser = require('body-parser');
 var app = express();
 var jsonParser = bodyParser.json();
 
+var lager = [
+    {
+        artikel: "Ball",
+        anzahl: 11,
+        preis: 9.99
+        },
+    {
+        artikel: "Zahnbürste",
+        anzahl: 45,
+        preis: 2.50
+        },
+    {
+        artikel: "Schreibtisch",
+        anzahl: 2,
+        preis: 79.99
+        },
+    {
+        artikel: "Schreibtischlampe",
+        anzahl: 19,
+        preis: 14.99
+        }
+        ];
+
 app.use(bodyParser.json());
 
 app.use(express.static('./public'));
 
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.end(err.status+' '+err.messages);
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.end(err.status + ' ' + err.messages);
 });
 
-app.use(function(req,res,next){
-  console.log('Time: %d '+' Request-Pfad: '+req.path, Date.now());
-  next();
+app.use(function (req, res, next) {
+    console.log('Time: %d ' + ' Request-Pfad: ' + req.path, Date.now());
+    next();
 });
 
 
-app.get('/warenkorb', function(req, res){
+app.get('/warenkorb', function (req, res) {
 
     res.writeHead(200, "OK");
-    res.write('jooooo');
+    res.write('Dies ist der Warenkorb.');
     res.end();
 });
 
-app.get('/warenkorb/:id', function(req, res){
+app.get('/warenkorb/:id', function (req, res) {
 
     if (req.params.id == 42) {
-         res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ a: 1 }, null, 3));
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({
+            a: 1
+        }, null, 3));
 
 
     } else {
@@ -41,11 +66,29 @@ app.get('/warenkorb/:id', function(req, res){
     res.end();
 });
 
-app.post('/users', jsonParser, function (req, res){
-  console.log(req.body);
-  res.writeHead(200, "OK");
-  res.write(req.body.user);
-  res.end();
+app.get('/lager', function (req, res) {
+
+    if (req.query.anzahl != undefined) {
+
+        var filteredData = lager.filter(function (value, index, arr) {
+            return value.anzahl == req.query.anzahl;
+        });
+
+        if (filteredData.length >= 1) {
+            res.status(200).send(filteredData);
+        } else {
+            res.status(404).send("Kein Artikel gefunden!").end();
+        }
+    } else {
+        res.json(lager);
+    }
+});
+
+app.post('/users', jsonParser, function (req, res) {
+    console.log(req.body);
+    res.writeHead(200, "OK");
+    res.write(req.body.user);
+    res.end();
 });
 
 
--- FabianDaugs/workshop_2/spezifikation.md
+++ FabianDaugs/workshop_2/spezifikation.md
@@ -5,6 +5,7 @@
 
 | Ressource      | Methode | Semantik                                                    | content-type (req) | content-type (res) |
 |----------------|---------|-------------------------------------------------------------|--------------------|--------------------|
-| /warenkorb     | GET     | Gibt "jooooo" zurrück                                        | text/plain         | text/plain         |
-| /warenkorb/:id | GET     | Gibt den User mit der entsprechenden Id zurrück             | text/plain         | text/plain         |
-| /users         | POST    | Gibt nur den User aus dem gepostetetn Jsondokument zurrück. | application/json   | text/plain          |
+| /warenkorb     | GET     | Gibt "Dies ist der Warenkorb" zurück                        | text/plain         | text/plain         |
+| /warenkorb/:id | GET     | Gibt den User mit der entsprechenden Id zurück              | text/plain         | text/plain         |
+| /users         | POST    | Gibt nur den User aus dem gepostetetn Jsondokument zurück.  | application/json   | text/plain         |
+| /lager         | GET     | Filtert nach Artikeln mit bestimmter Anzahl im Lager.       | application/json   | text/plain         |
