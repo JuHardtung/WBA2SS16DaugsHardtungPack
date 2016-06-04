ef7ea6ed921d1fba5d0b2fcfd384df65dc6fee2f
Nachtigoil <trojan9418@gmail.com>
2016-06-04 16:41:00 +0200

    ArticlesController  to ArticleController

--- Projekt/router.js
+++ Projekt/router.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Response = require('./helper/ResponseHelper.js');
 
-var ArticlesController = require('./controllers/ArticlesController');
+var ArticleController = require('./controllers/ArticlesController');
 var AuthController = require('./controllers/AuthController');
 var CartController = require('./controllers/CartController');
 var CartController = require('./controllers/UserController');
