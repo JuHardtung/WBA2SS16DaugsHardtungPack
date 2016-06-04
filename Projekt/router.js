2e301647f9cedac44d8918f03af88bf6dda5afbc
fdaugs <fabian.daugs@smail.th-koeln.de>
2016-06-04 16:38:15 +0200

    User Routen hinzugefügt

--- Projekt/router.js
+++ Projekt/router.js
@@ -1,18 +1,19 @@
-var express 	= require('express');
-var router 		= express.Router();
-var Response  = require('./helper/ResponseHelper.js');
+var express = require('express');
+var router = express.Router();
+var Response = require('./helper/ResponseHelper.js');
 
-var ArticlesController    = require('./controllers/ArticlesController');
-var AuthController   = require('./controllers/AuthController');
-var CartController   = require('./controllers/CartController');
+var ArticlesController = require('./controllers/ArticlesController');
+var AuthController = require('./controllers/AuthController');
+var CartController = require('./controllers/CartController');
+var CartController = require('./controllers/UserController');
 
 router.route('/*')
-  .trace(function(req, res, next) {
-    res.send(Response.successfull(200, req.body, null, null));
-  })
-  .options(function(req, res, next) {
-    res.send(Response.successfull(100));
-  });
+    .trace(function(req, res, next) {
+        res.send(Response.successfull(200, req.body, null, null));
+    })
+    .options(function(req, res, next) {
+        res.send(Response.successfull(100));
+    });
 
 // Articles
 router.route('/signup')
@@ -35,6 +36,18 @@ router.route('/cart/:id/add')
 router.route('/cart/:id/delete')
     .get(CartController.deleteItem);
 
+//User
+router.route('/user/all')
+    .get(UserController.getAll);
+
+router.route('/user/:id')
+    .get(UserController.getUser);
+
+router.route('/user/add')
+    .get(UserController.add);
+
+router.route('/user/delete')
+    .get(UserController.delete);
 
 // Finally export the router
 module.exports = router;
