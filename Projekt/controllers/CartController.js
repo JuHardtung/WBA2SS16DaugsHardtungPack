3a84ed9bdf294fb5f95bcc1f48e9913063ee2e82
Trojan13 <trojan9418@gmail.com>
2016-06-19 16:27:58 +0200

    changed addItem to accept Json as post

--- Projekt/controllers/CartController.js
+++ Projekt/controllers/CartController.js
@@ -5,6 +5,10 @@ function checkshoppingcartId() {
     return true;
 }
 
+function checkValidItem() {
+    return true;
+}
+
 module.exports = {
 
     /**
@@ -40,15 +44,19 @@ module.exports = {
      * @param {int} quantity
      */
     addItem: function (req, res, next) {
-        var itemid = req.query.itemid;
-        var quantity = req.query.quantity;
-        console.log(quantity);
+        var json = req.body;
+        json.quantity = parseInt(json.quantity);
+        if (!isNaN(parseInt(json.quantity)) && checkValidItem(json.itemid)){
         if (checkshoppingcartId()) {
-            redisClient.rpush("shoppingcart:" + req.params.id, itemid + ":" + quantity);
+            redisClient.rpush("shoppingcart:" + req.params.id, json);
             res.write('Item added.');
             res.end();
         } else {
             res.write('User not found.');
+            res.end();
+        } 
+        } else {
+            res.write('JSON Format Error');
             res.end();
         }
     },
