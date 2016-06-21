a81fc76f290d946d9239726c6ba9abdde44ae602
Trojan13 <trojan9418@gmail.com>
2016-06-21 14:04:54 +0200

    removed isValidUser

--- Projekt/controllers/UserController.js
+++ Projekt/controllers/UserController.js
@@ -2,7 +2,7 @@ var redis = require('redis');
 var client = redis.createClient();
 var Response = require('../helper/ResponseHelper');
 var httpStatus = require('http-status-codes');
-
+var expressValidator = require('express-validator');
 
 function errorInDatabase(res, err) {
     if (err !== null) {
@@ -14,31 +14,9 @@ function errorInDatabase(res, err) {
 
 }
 
-function isValidUser(user) {
-    if (user === undefined) {
-        return false;
-    }
-    if (user.id === undefined) {
-        return false;
-    }
-    if (user.vorname === undefined) {
-        return false;
-    }
-    if (user.nachname === undefined) {
-        return false;
-    }
-    if (user.email === undefined) {
-        return false;
-    }
-    if (user.passwort === undefined) {
-        return false;
-    }
-    return true;
-}
-
 function getUserById(userList, id) {
     var result;
-    userList.forEach(function(entry) {
+    userList.forEach(function (entry) {
         _json = JSON.parse(entry);
         if (parseInt(_json.id) == id) {
             result = JSON.stringify(_json);
@@ -51,24 +29,24 @@ function getUserById(userList, id) {
 
 module.exports = {
 
-    getAll: function(req, res, next) {
-        client.lrange('USER', 0, -1, function(err, reply) {
+    getAll: function (req, res, next) {
+        client.lrange('USER', 0, -1, function (err, reply) {
             if (!errorInDatabase(res, err)) {
                 console.log("Bekomme alle User!");
                 if (reply === null) {
                     res.status(httpStatus.NOT_FOUND);
                 } else {
-                    res.status(httpStatus.OK).end(reply+ "");
+                    res.status(httpStatus.OK).end(reply + "");
                 }
             }
         });
     },
-    
-    getUser: function(req, res, next) {
+
+    getUser: function (req, res, next) {
 
         var _userId = parseInt(req.query.userId);
 
-        client.lrange('USER', 0, -1, function(err, reply) {
+        client.lrange('USER', 0, -1, function (err, reply) {
             if (!errorInDatabase(res, err)) {
                 console.log("Bekomme User mit der ID " + _userId);
 
@@ -82,8 +60,18 @@ module.exports = {
         });
     },
 
-    add: function(req, res, next) {
-        client.llen('USER', function(err, reply) {
+    add: function (req, res, next) {
+        req.checkBody('user.id', 'Invalid UserId').notEmpty().isInt();
+        req.checkBody('user.vorname', 'Invalid Vorname').notEmpty().isString();
+        req.checkBody('user.nachname', 'Invalid Nachname').notEmpty().isString();
+        req.checkBody('user.email', 'Invalid email').notEmpty().isString();
+        req.checkBody('user.passwort', 'Invalid password').notEmpty().isString();
+        var errors = req.validationErrors();
+        if (errors) {
+            res.status(400).send('There have been validation errors: ' + util.inspect(errors));
+            return 0;
+        }
+        client.llen('USER', function (err, reply) {
             if (!errorInDatabase(res, err)) {
                 var _id;
                 if (reply === null) {
@@ -93,25 +81,23 @@ module.exports = {
                 }
                 var newUSER = req.body;
 
-                if (isValidUser(newUSER) === true) {
-                    client.rpush('USER', JSON.stringify(newUSER), function(err, reply) {
-                        if (!errorInDatabase(res, err)) {
-                            if (reply !== _id) {
-                                console.log("Neuen User mit ID " + _id + " erstellt.");
-                                res.status(httpStatus.OK).json(newUSER);
-                            }
+                client.rpush('USER', JSON.stringify(newUSER), function (err, reply) {
+                    if (!errorInDatabase(res, err)) {
+                        if (reply !== _id) {
+                            console.log("Neuen User mit ID " + _id + " erstellt.");
+                            res.status(httpStatus.OK).json(newUSER);
                         }
-                    });
-                }
+                    }
+                });
             }
         });
     },
 
-    delete: function(req, res, next) {
+    delete: function (req, res, next) {
         var _userId = parseInt(req.query.userId);
         console.log(_userId);
 
-        client.lrange('USER', 0, -1, function(err, reply) {
+        client.lrange('USER', 0, -1, function (err, reply) {
             if (!errorInDatabase(res, err)) {
                 console.log("Lösche User mit der ID: " + _userId);
 
