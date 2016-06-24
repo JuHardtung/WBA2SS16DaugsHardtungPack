4a236a90d7a3b37885a6bdd810d7c505d54deee6
Trojan13 <trojan9418@gmail.com>
2016-06-24 14:08:31 +0200

    fixxed fabsis shice

--- Projekt/client/controllers/CartController.js
+++ Projekt/client/controllers/CartController.js
@@ -10,19 +10,19 @@ module.exports = {
     getCart: function (req, res, next) {
 
         var options = {
-            uri: 'http://127.0.0.1:3000/cart/' + req.session.userId,
-            headers: {
+            uri: 'http://127.0.0.1:3000/cart/' + req.session.userId
+            , headers: {
                 'User-Agent': 'Request-Promise'
-            },
-            json: true // Automatically parses the JSON string in the response
+            }
+            , json: true // Automatically parses the JSON string in the response
         };
 
         rp(options)
             .then(function (response) {
                 var data = {
-                    title: 'Warenkorb',
-                    articles: response,
-                    session: req.session
+                    title: 'Warenkorb'
+                    , articles: response
+                    , session: req.session
                 };
 
                 res.render('articles/cart', data);
@@ -30,9 +30,9 @@ module.exports = {
             })
             .catch(function (err) {
                 var data = {
-                    message: "Artikel konnten nicht angezeigt werden!",
-                    code: res.statusCode,
-                    validation: res.validation
+                    message: "Artikel konnten nicht angezeigt werden!"
+                    , code: res.statusCode
+                    , validation: res.validation
                 };
                 res.render('error', err);
                 console.log(err);
@@ -42,39 +42,39 @@ module.exports = {
     },
 
 
-    addItem: function(req, res, next) {
+    addItem: function (req, res, next) {
         var id = req.body.id;
         var qty = req.body.qty;
         var options = {
-            uri: 'http://127.0.0.1:3000/cart/'+req.session.userId,
-            method: 'POST',
-            headers: {
-                'User-Agent': 'Request-Promise',
-                'Content-Type': 'application/json; charset=utf-8',
-                'Content-Length': {
-                    "id": id,
-                    "qty": qty,
-                }.length
-            },
-            body: {
-                "id": id,
-                "qty": qty,r
-            },
-            json: true // Automatically parses the JSON string in the response
+            uri: 'http://127.0.0.1:3000/cart/' + req.session.userId
+            , method: 'POST'
+            , headers: {
+                'User-Agent': 'Request-Promise'
+                , 'Content-Type': 'application/json; charset=utf-8'
+                , 'Content-Length': {
+                    "id": id
+                    , "qty": qty
+                , }.length
+                , body: {
+                    "id": id
+                    , "qty": qty
+                }
+                , json: true // Automatically parses the JSON string in the response
+            }
         };
 
         rp(options)
             .then(function (response) {
                 var data = {
-                    title: 'Warenkorb',
-                    articles: response,
-                    session: req.session
+                    title: 'Warenkorb'
+                    , articles: response
+                    , session: req.session
                 };
 
                 res.send("OK");
 
             })
-            .catch(function(err) {
+            .catch(function (err) {
                 res.send("Fail");
             });
 
