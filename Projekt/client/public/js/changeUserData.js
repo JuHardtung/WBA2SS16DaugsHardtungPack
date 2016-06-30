02ed2b28324cb8d736c8ce99e8e80ada3fc88702
JuHardtung <julian.hardtung@smail.fh-koeln.de>
2016-06-30 11:48:07 +0200

    fixed bugs at userSettings (empty inputs; multiple answers; wrong colors)

--- Projekt/client/public/js/changeUserData.js
+++ Projekt/client/public/js/changeUserData.js
@@ -1,42 +1,64 @@
 $("#changePwd").click(function () {
-                var password = $("#inputPassword").val();
-                if (password == '') {
-                    $('#inputPassword').css("border", "2px solid red");
-                    $('#inputPassword').css("box-shadow", "0 0 3px red");
-                }
-                $.post("/settings", {
+    var password = $("#inputPassword").val();
 
-                        password: password,
-                    },
-                    function (data) {
-                        if (data == 'password change failed') {
-                            $('#password').css("border", "2px solid red");
-                            $('#password').css("box-shadow", "0 0 3px red");
-                            $('#password').after('<p class="user">Password wechseln nicht erfolgreich!</p>');
+    if (password == '') {
+        $('#answerPwdOK').remove();
+        $('#inputPassword').css("border", "2px solid red");
+        $('#inputPassword').css("box-shadow", "0 0 3px red");
+        $('#changePwd').after('<p id="answerPwdFailed">Geben Sie bitte ein Passwort an!</p>');
+        $('#answerPwdFailed').css("color", "red");
+        return;
+    }
+    $.post("/settings", {
 
-                        } else {
-                            $('#changePwd').after('<p class="signup">Password ändern erfolgreich!</p>');
-                        }
-                    });
-            });
-            $("#changeMail").click(function () {
-                var mail = $("#inputMail").val();
-                if (mail == '') {
-                    $('#inputMail').css("border", "2px solid red");
-                    $('#inputMail').css("box-shadow", "0 0 3px red");
-                }
-                $.post("/settings", {
+            password: password,
+        },
+        function (data) {
+            if (data == 'password change failed') {
+                $('#answerPwdOK').remove();
+                $('#inputPassword').css("border", "2px solid red");
+                $('#inputPassword').css("box-shadow", "0 0 3px red");
+                $('#changePwd').after('<p id="answerPwdFailed">Password wechseln nicht erfolgreich!</p>');
+                $('#answerPwdFailed').css("color", "red");
 
-                        mail: mail,
-                    },
-                    function (data) {
-                        if (data == 'mail change failed') {
-                            $('#mail').css("border", "2px solid red");
-                            $('#mail').css("box-shadow", "0 0 3px red");
-                            $('#mail').after('<p class="user">Mail wechseln nicht erfolgreich!</p>');
+            } else {
+                $('#answerPwdFailed').remove();
+                $('#inputPassword').css("border", "2px solid green");
+                $('#inputPassword').css("box-shadow", "0 0 3px green");
+                $('#changePwd').after('<p id="answerPwdOK">Password ändern erfolgreich!</p>');
+                $('#answerPwdOK').css("color", "green");
+            }
+        });
+});
+$("#changeMail").click(function () {
+    var mail = $("#inputMail").val();
 
-                        } else {
-                            $('#changeMail').after('<p class="signup">Mail ändern erfolgreich!</p>');
-                        }
-                    });
-            });
+    if (mail == '') {
+        $('#answerMailOK').remove();
+        $('#inputMail').css("border", "2px solid red");
+        $('#inputMail').css("box-shadow", "0 0 3px red");
+        $('#changeMail').after('<p id="answerMailFailed">Geben Sie bitte eine E-Mail-Adresse an!</p>');
+        $('#answerMailFailed').css("color", "red");
+        return;
+    }
+    $.post("/settings", {
+
+            mail: mail,
+        },
+        function (data) {
+            if (data == 'mail change failed') {
+                $('#answerMailOK').remove();
+                $('#inputMail').css("border", "2px solid red");
+                $('#inputMail').css("box-shadow", "0 0 3px red");
+                $('#changeMail').after('<p id="answerMailFailed">Mail wechseln nicht erfolgreich!</p>');
+                $('#answerMailFailed').css("color", "red");
+
+            } else {
+                $('#answerMailFailed').remove();
+                $('#inputMail').css("border", "2px solid green");
+                $('#inputMail').css("box-shadow", "0 0 3px green");
+                $('#changeMail').after('<p id="answerMailOK">Mail ändern erfolgreich!</p>');
+                $('#answerMailOK').css("color", "green");
+            }
+        });
+});
